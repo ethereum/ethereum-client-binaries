0994cec00c7039078de4732e99dd9a817b060f19
Ramesh Nair <ram@hiddentao.com>
2016-09-18 15:22:16 +0800

    work towards scanning

--- src/index.js
+++ src/index.js
@@ -1,2 +1,78 @@
 "use strict";
 
+const got = require('got');
+  
+
+const DUMMY_LOGGER = {
+  debug: function() {},
+  info: function() {},
+  warn: function() {},
+  error: function() {}
+};
+
+
+const DefaultConfig = exports.DefaultConfig = require('./config.json');
+
+
+class Manager {
+  /**
+   * Construct a new instance.
+   *
+   * @param {Object} [config] The configuraton to use. If ommitted then the 
+   * default configuration (`DefaultConfig`) will be used.
+   */
+  constructor (config) {
+    this._config = config || DefaultConfig;
+    this._logger = DUMMY_LOGGER;
+  }
+  
+  
+  /**
+   * Set the logger.
+   * @param {Object} val Should have same methods as global `console` object.
+   */
+  set logger (val) {
+    this._logger = {};
+    
+    for (let key in DUMMY_LOGGER) {
+      this._logger[key] = (val && typeof val[key] === 'function') 
+        ? val[key].bind(val)
+        : DUMMY_LOGGER[key]
+      ;
+    }
+  }
+
+  
+  /**
+   * Initialise the manager.
+   *
+   * @return {Promise}
+   */
+  init() {
+    this._logger.info('Initializing...');
+    
+    return Promise.resolve()
+      .then(() => this._scan());
+  }
+  
+  
+  /**
+   * Scan the local machine for client software, as defined in the configuration.
+   *
+   * Upon completion `this._state` will be set.
+   *
+   * @return {Promise}
+   */
+  _scan () {
+    
+  }
+  
+}
+
+
+exports.Manager = Manager;
+
+
+
+
+
